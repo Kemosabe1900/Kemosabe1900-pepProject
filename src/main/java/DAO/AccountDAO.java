9318d194479d2f3278694019694e759673b3f88e
Kemosabe1900 <62734465+Kemosabe1900@users.noreply.github.com>
2023-07-11 18:56:23 -0500

    edits

---
+++ src/main/java/DAO/AccountDAO.java
@@ -0,0 +1,15 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import Model.Account;
+import util.ConnectionUtil;
+
+public class AccountDAO{
+    private Connection connection;
+    
+}
